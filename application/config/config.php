55fb0d637b548a480a26c7bd780e15c7a221e2a6
Tess Robertson <theresa.n.robertson@gmail.com>
2016-10-07 15:11:30 -0700

    Fixed menu bar Signed-off-by:Tess Robertson <theresa.n.robertson@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -4,11 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // the menu basics (text navbar)
 $config['menu_choices'] = array(
     'menudata' => array(
-        array('name' => 'Homepage', 'link' => '/homepage'),
-        array('name' => 'Production', 'link' => '/production'),
-        array('name' => 'Receiving', 'link' => '/receiving'),
-        array('name' => 'Administration', 'link' => '/administration'),
-        array('name' => 'Sales', 'link' => '/sales')
+        array('name' => 'Recipes', 'link' => '/recipes'),
+        array('name' => 'Stock', 'link' => '/stock'),
+        array('name' => 'Supplies', 'link' => '/supplies')
         )
     );
 
