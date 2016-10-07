77cf684ddc16b98c6fc1a505e37c3aa853443525
Thach Le <A00926906@ad.bcit.ca>
2016-10-07 12:31:13 -0700

    fix receiving view & controller to work with new model
    Signed-off-by: Thach Le <A00926906@ad.bcit.ca>

--- application/controllers/Supply.php
+++ application/controllers/Supply.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * @author Thach
  */
-class Receiving extends Receiving
+class Receiving extends Application
 {
         /**
          * Default Constructor
--- application/views/receiving.php
+++ application/views/receiving.php
@@ -2,7 +2,8 @@
 	<center><h4>Inventory.</h4></center><br>
 	{supplies}
 	<div class="span3x">
-		<p>{code}<p></br>
+		<p>{name}<p></br>
+		<p>{description}<p></br>
 		<form action="/Receiving/receipt">
 			<label>Order </label>
 			<input type="text" name="receiving_unit">
