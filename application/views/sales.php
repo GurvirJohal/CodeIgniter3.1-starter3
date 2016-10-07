f13c0a5687d7f50561c9a55ceeaebf43abcc96ab
j <j@j-PC>
2016-10-06 20:58:24 -0700

    updates sales view Signed-off-by:j <j@j-PC>

--- application/views/sales.php
+++ application/views/sales.php
@@ -1,11 +1,25 @@
 <div class="row">
     <center><h4>Welcome to our catalog!</h4></center><br>
-    {stock}
-    <div class="span3x">
-        <p>{code} - {price}</p><br/>
-        <p>{description}</p><br/>
-        <p>{quantity} left in stock</p><br/>
-    </div>
-    
-    {/stock}
+    <form action="">
+        {stock}
+        <div class="span3x">
+            <p>{code} - {price}</p><br/>
+            <p>{description}</p><br/>
+            <p>{quantity} left in stock</p><br/>
+            <table align="center">
+                <tr align="center">
+                    <td>Add to cart</td>
+                    <td><input type="checkbox" name="cart" value="{code}"></td>
+                </tr>
+                <tr align="center">
+                    <td><p>Quantity</p></td>
+                    <td><input type="text" name="quantity" value="1" style="width:20px"></td>
+                </tr>
+            </table>
+            
+            
+        </div>
+
+        {/stock}
+    </form>
 </div>
