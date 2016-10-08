0992bf891609f27f3913da4e2ae82ca8934ee7af
Thach Le <A00926906@ad.bcit.ca>
2016-10-08 15:06:25 -0700

    Associated the function to run on page render and show in the correct position
    Signed-off-by: Thach Le <A00926906@ad.bcit.ca>

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,6 +1,6 @@
 <div class="row">
     <h4>Dashboard</h4><br>
-    <p>$ spent purchasing inventory: </p>
+    <p>$ spent purchasing inventory: {totalCost}</p>
     <p>$ received from sales: </p>
     <p>cost of sales ingredients consumed: </p>
 </div>
