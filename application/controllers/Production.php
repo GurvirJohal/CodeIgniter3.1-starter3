42d7c979fadc6517e49f86aea1df8ea9c0443109
jayguyhaych <jayguyhaych@inbox.lv>
2016-10-08 16:41:55 -0700

    - Listing Ingredients and amount for Recipe in progress. Only a single ingredient and amount is showing currently. Signed-off-by:jayguyhaych <jayguyhaych@inbox.lv>

--- application/controllers/Production.php
+++ application/controllers/Production.php
@@ -24,13 +24,15 @@ class Production extends Application
             // build the list of authors, to pass on to our view
             $source = $this->recipes->all();
             $recipes = array ();
+            //$recipIngre = array ();
             foreach ($source as $record)
             {
                 $recipes[] = array ('code' => $record['code'],
-                                    'name' => $record['name']
+                                    'name' => $record['name'],
+                                    'ingredients' => $record['ingredients']
                                    );
                 // ~Not sure about this part!!!!
-                $recipIngre[] = array ('ingredients' => $record['ingredients']);
+                //$recipIngre[] = array ('ingredients' => $record['ingredients']);
             }
             
             $this->data['recipes'] = $recipes;
@@ -38,11 +40,32 @@ class Production extends Application
             $this->render();
 	}
         
+        
         public function gimme($id)
         {
-            $this->data['pagebody'] = 'ingredients';	//views/ingridents	
-            $source = $this->recipes->get($id);
-            $this->data = array_merge($this->data, $source);
+            $this->data['pagebody'] = 'ingredients';
+            
+            $recipe = $this->recipes->get($id);
+            $source = $this->recipes->getIngre($id);
+            
+            $this->data = array_merge($this->data, $source, $recipe);
             $this->render();
         }
+        
+/**
+        public function get($code){
+            //the pagebody of the ingredients and amounts to make an item/recipe
+            $this->data['pagebody'] = 'ingredients';
+            
+            //
+            $source = $this->recipes->get($code);
+            $recipes[] = array('id' => $source['code'],
+                                              'name' => $source['name'],
+                                              'description' => $source['dscription'],
+                                              'ingredients' => $source['ingredients']);  
+            $this->data['recipes'] = $recipes;
+            $this->render();
+        }
+ * 
+ */
 }
